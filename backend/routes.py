27c7736cc35358957e7574b5e42a8167f3d08273
Tamanna <tamanna.11507@gmail.com>
2023-11-16 10:47:24 +0100

    changed api to return json values

--- backend/routes.py
+++ backend/routes.py
@@ -1,14 +1,20 @@
-from flask import Blueprint
-import pandas as pd 
+import csv
 
-df = pd.read_csv('Data/zurich.csv')
+from flask import Blueprint, jsonify
 
+csv_file_path = 'Data/zurich.csv'
 
 all_routes = Blueprint('all_routes', __name__)
 
 @all_routes.route('/', methods=['GET'])
 def start_page():
-    text = df.to_string()
-    return text
+    csv_encoding = 'utf-8'
+    data = []
+    with open(csv_file_path, 'r', encoding=csv_encoding) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data.append(row)
+    return jsonify(data)
+
 
 
