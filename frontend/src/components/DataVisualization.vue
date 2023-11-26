1f760f184a1090dc0cbd5daa770ea2bdfda6da3e
varunghat <varungr2540@gmail.com>
2023-11-26 16:25:11 +0100

    Added histograms

--- frontend/src/components/DataVisualization.vue
+++ frontend/src/components/DataVisualization.vue
@@ -2,22 +2,22 @@
   <v-container width="200%">
    <v-row class="bg-grey-lighten-2">
      <v-col cols="8">
-       <v-sheet class="pa-2 ma-2" height="720px" id="mapContainer"> Level 1: .v-col-8 </v-sheet>
+       <v-sheet class="pa-2 ma-2" height="720px" id="mapContainer"  @click:map="update_map"></v-sheet>
      </v-col>
      <v-col cols="4">
        <v-row no-gutters>
          <v-col cols="12">
-           <v-sheet class="pa-2 ma-2" height="233px"> Level 1: .v-col-12 </v-sheet>
+           <v-sheet class="pa-2 ma-2" height="233px" id="histContainer1"></v-sheet>
          </v-col>
        </v-row>
        <v-row no-gutters >
          <v-col cols="12">
-           <v-sheet class="pa-2 ma-2" height="233px"> Level 2: .v-col-12 </v-sheet>
+           <v-sheet class="pa-2 ma-2" height="233px" id="histContainer2"></v-sheet>
          </v-col>
        </v-row>
        <v-row no-gutters >
          <v-col cols="12">
-           <v-sheet class="pa-2 ma-2" height="233px"> Level 3: .v-col-12 </v-sheet>
+           <v-sheet class="pa-2 ma-2" height="233px" id="histContainer3"></v-sheet>
          </v-col>
        </v-row>
      </v-col>
@@ -26,76 +26,70 @@
  </template>
  
  <script>
-   // Import Leaflet for map visualization
-   import "leaflet/dist/leaflet.css";
-   import L from 'leaflet';
+  // Import Leaflet for map visualization
+  import "leaflet/dist/leaflet.css";
+  import L from 'leaflet';
 
-   import Plotly from 'plotly.js/dist/plotly';
+  import Plotly from 'plotly.js-dist';
 
-   //import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet"
+  //import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet"
 
-   //Vue.component('l-map', LMap);
-   //Vue.component('l-tile-layer', LTileLayer);
-   //Vue.component('l-marker', LMarker);
-   delete L.Icon.Default.prototype._getIconUrl;
+  //Vue.component('l-map', LMap);
+  //Vue.component('l-tile-layer', LTileLayer);
+  //Vue.component('l-marker', LMarker);
+  delete L.Icon.Default.prototype._getIconUrl;
 
-   L.Icon.Default.mergeOptions({
-     iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-     iconUrl: require('leaflet/dist/images/marker-icon.png'),
-     shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-   });
- 
-   export default {
-   name: 'DataVisualization',
-   props: {
-     msg: String
-   },
-   data: () => ({
-       loading: false,
-       elevations: [0, 4, 8, 12, 16, 20],
-       predictedData: [],
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  });
 
-     }),
- 
-   mounted() {
-     this.fetchData()
+  export default {
+    name: 'DataVisualization',
+    props: {
+      msg: String
+    },
+    data: () => ({
+        loading: false,
+        elevations: [0, 4, 8, 12, 16, 20],
+        predictedData: [],
+        number_of_reviews: [],
+        minimum_nights: [],
+        price: [],
+      }),
+  
+    mounted() {
+      console.log("Fetching data")
+      this.fetchData()
 
-     this.map = L.map("mapContainer").setView([47.37, 8.55], 12);
+      this.map = L.map("mapContainer").setView([47.37, 8.55], 12);
 
-     L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-       attribution:
-         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-     }).addTo(this.map);
-     //use a mix of renderers
-     var customPane = this.map.createPane("customPane");
-     //var canvasRenderer = L.canvas({ pane: "customPane" });
-     customPane.style.zIndex = 399; // put just behind the standard overlay pane which is at 400
+      L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(this.map);
+      //use a mix of renderers
+      var customPane = this.map.createPane("customPane");
+      //var canvasRenderer = L.canvas({ pane: "customPane" });
+      customPane.style.zIndex = 399; // put just behind the standard overlay pane which is at 400
 
-     // Dummy data values, change once dataloading is done
+      // Dummy data values, change once dataloading is done
 
-     var apt_list = [[47.37,8.55],[47.37374,8.51],[47.3738,8.81],[47.37171,8.41],[47.37324,8.23]];
-     var price_list = [1200,900,200,300,500]
-     var rank_list = [0,3,6,9,30]
-     var titles = ["Cozy apartment in zurich","Shitty apartment","Apt3","Apt4","Apt5"]
-     var max_rank = Math.max(...rank_list)
-     
-     
-     var i;
-     for(i=0;i<apt_list.length;i++)
-     {
-       var marker = L.marker(apt_list[i],{title :`${price_list[i]} CHF`}).addTo(this.map);
-       marker.bindPopup(`<b> ${titles[i]} </b><br>Price: <b>${price_list[i]} CHF</b><br>Rank: <b>${rank_list[i]}</b>`);
-     
-       var hue_rotate_val = 250-100*(rank_list[i]/max_rank);
-       marker._icon.style.filter = `hue-rotate(${hue_rotate_val}deg)`
-     }
 
-     this.map.on('moveend', () => {
-       const bounds = this.map.getBounds().toBBoxString();
-       /* now send your bounds to the server, requesting only the visible markers */
-       console.log(bounds)
-     })
-     },
+      //var apt_list = [[47.37,8.55],[47.37374,8.51],[47.3738,8.81],[47.37171,8.41],[47.37324,8.23]];
+      //var price_list = [1200,900,200,300,500]
+      //var rank_list = [0,3,6,9,30]
+      //var titles = ["Cozy apartment in zurich","Shitty apartment","Apt3","Apt4","Apt5"]
+
+      
+      this.map.on('moveend', () => {
+        const bounds = this.map.getBounds().toBBoxString();
+        /* now send your bounds to the server, requesting only the visible markers */
+        console.log(bounds)
+      })
+    },
+
     methods: {
       async fetchData() {
         const requestOptions = {
@@ -105,15 +99,68 @@
         const response =  await fetch("http://127.0.0.1:5000/model-predict", requestOptions)
         const responseData =  await response.json();
         responseData.forEach((apartment) => {
-            this.predictedData.push(apartment)
+            this.predictedData.push(apartment);
+            this.number_of_reviews.push(apartment["number_of_reviews"]);
+            this.minimum_nights.push(apartment["minimum_nights"]);
+            this.price.push(apartment["price"]);
+            
       })
-      console.log(this.predictedData)
-      }
+        //console.log(this.predictedData.length)
+        this.update_map();
+        this.drawHistogramPlot("histContainer1",this.price);
+        this.drawHistogramPlot("histContainer2",this.minimum_nights);
+        this.drawHistogramPlot("histContainer3",this.number_of_reviews);
+      },
+
+      update_map() {
+
+        //console.log(props)
+        var max_rank = Math.min(this.predictedData.length,100);
+      
+      
+        var i;
+
+
+        console.log("Predicted data:")
+        console.log(this.predictedData)
+        console.log(this.predictedData.length)
+
+
+        for(i=0;i<max_rank;i++)
+        {
+          var price = this.predictedData[i]["price"]
+          var latitude = this.predictedData[i]["latitude"]
+          var longitude = this.predictedData[i]["longitude"]
+          var title = this.predictedData[i]["name"]
+          var rank = i
+
+          console.log(this.predictedData[i]["rating"])
+          var marker = L.marker([latitude,longitude],{title :`${price} CHF`}).addTo(this.map);
+          marker.bindPopup(`<b> ${title} </b><br>Price: <b>${price} CHF</b><br>Rank: <b>${rank}</b>`);
+        
+          var hue_rotate_val = 250-100*(rank/max_rank);
+          marker._icon.style.filter = `hue-rotate(${hue_rotate_val}deg)`
+        }
+
+      },
+
+      drawHistogramPlot(id,hist_data) {
+        var trace1 = {
+          x: hist_data,
+          type: 'histogram',
+          //text: this.ScatterPlotData.name,
+
+        };
+        var data = [trace1];
+        var layout = {}
+        var config = {responsive: true}
+        Plotly.newPlot(id, data, layout, config);
+        //this.clickScatterPlot()
+      },
     }
-    }
-  
   }
- 
+
+
  </script>
 
  <!-- Add "scoped" attribute to limit CSS to this component only -->
